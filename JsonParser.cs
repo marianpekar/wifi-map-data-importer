4c81178bc2b1f11058702776962061dbc6a74e80
Marian Pekár <marian.pekar@gmail.com>
2024-11-24 11:23:39 +0100

    Add support for multiple source JSON files; merge during import.

--- JsonParser.cs
+++ JsonParser.cs
@@ -7,12 +7,11 @@ namespace WiFiMapDataImporter;
 
 public static class JsonParser
 {
-    public static Network[] DeserializeWiFiMapData(string filePath)
+    public static Network[] ParseWiFiMapData(string[] filePaths)
     {
-        var json = File.ReadAllText(filePath);
-        var jsonObject = JObject.Parse(json);
-        var networks = new List<Network>();
+        JObject jsonObject = ReadAndMerge(filePaths);
 
+        var networks = new List<Network>();
         foreach (var networkEntry in jsonObject)
         {
             var network = new Network(networkEntry.Key)
@@ -47,5 +46,53 @@ public static class JsonParser
         }
         
         return networks.ToArray();
+    }
+
+private static JObject ReadAndMerge(string[] filePaths)
+    {
+        string json = File.ReadAllText(filePaths[0]);
+        JObject jsonObject = JObject.Parse(json);
+
+        for (int i = 1; i < filePaths.Length; i++)
+        {
+            string nextJson = File.ReadAllText(filePaths[i]);
+            JObject nextJsonObject = JObject.Parse(nextJson);
+
+            foreach (var nextProperty in nextJsonObject.Properties())
+            {
+                string key = nextProperty.Name;
+                
+                if (jsonObject.TryGetValue(key, out var value))
+                {
+                    var currentValue = value as JObject;
+                    var newValue = nextJsonObject[key] as JObject;
+
+                    if (currentValue != null && newValue != null)
+                    {
+                        foreach (var newProperty in newValue.Properties())
+                        {
+                            if (newProperty.Value.HasValues || !string.IsNullOrEmpty(newProperty.Value.ToString()))
+                            {
+                                currentValue[newProperty.Name] = newProperty.Value;
+                            }
+                        }
+                        
+                        if (currentValue["levels"] is JObject existingLevels && newValue["levels"] is JObject newLevels)
+                        {
+                            foreach (var level in newLevels.Properties())
+                            {
+                                existingLevels[level.Name] = level.Value;
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    jsonObject[key] = nextProperty.Value;
+                }
+            }
+        }
+
+        return jsonObject;
     }
 }
--- Program.cs
+++ Program.cs
@@ -9,13 +9,13 @@ namespace WiFiMapDataImporter
         {
             if (args.Length < 2)
             {
-                Console.WriteLine("Usage: WiFiMapDataImporter <jsonFilePath> <mongoUrl (i.e. mongodb://localhost:27017)>");
+                Console.WriteLine("Usage: WiFiMapDataImporter <mongoUrl (i.e. mongodb://localhost:27017)> <jsonFilePath1> (<jsonFilePath2> ...)");
                 return;
             }
 
-            Network[] networks = JsonParser.DeserializeWiFiMapData(args[0]);
+            Network[] networks = JsonParser.ParseWiFiMapData(args[1..]);
             
-            var mongoUrl = args[1];
+            var mongoUrl = args[0];
             new Importer(mongoUrl).ImportNetworks(
                 networks,
                 new Importer.ImportOptions
